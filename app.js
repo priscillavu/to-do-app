ff21e67d0529b48ebf855e0f0c674d9a82badc1c
Priscilla <priscilla.vu@aol.com>
2020-05-16 01:39:07 -0700

    setting up to-do app

---
+++ app.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    alert("The window has loaded!");
+    onReady();
+};
+
+function onReady() {
+    const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
+}
+
+window.onload = function() {
+
+}
